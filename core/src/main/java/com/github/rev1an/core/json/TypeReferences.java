e9ef2a4a024e7d8d58f8dc504933631bd79d2a53
Sergey Alexeyev <32193619+rev1an@users.noreply.github.com>
2022-07-16 15:48:07 +0400

    Added 'jackson-databind' dependency to 'core' module, added JacksonMapper as extension for ObjectMapper

---
+++ core/src/main/java/com/github/rev1an/core/json/TypeReferences.java
@@ -0,0 +1,28 @@
+/*
+ * (C) Copyright 2022 Sergey Alekseev (https://github.com/rev1an).
+ *
+ * Software distributed under MIT license.
+ */
+
+package com.github.rev1an.core.json;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.util.Map;
+
+/**
+ * Container for Jackson {@link TypeReference}'s
+ *
+ * @author rev1an (Sergey Alekseev)
+ */
+public final class TypeReferences {
+
+    // @formatter:off
+    public static final TypeReference<Map<String, Object>> MAP_STRING_OBJECT = new TypeReference<>(){};
+    public static final TypeReference<Map<String, Map<String, Object>>> MAP_STRING_MAP = new TypeReference<>(){};
+    // @formatter:on
+
+    private TypeReferences() {
+    }
+
+}
